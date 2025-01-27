653a4cf49e7ec5f3f40103e095cffa921d013110
RedGindew <redgindew@gmail.com>
2025-01-27 20:18:12 +0000

    1.5

--- PartyNightKit/PartyNightKit.cs
+++ PartyNightKit/PartyNightKit.cs
@@ -31,10 +31,11 @@ public class PartyNightKit : Mod
 
     // visual data about this mod
     public override string Name => "Party Night Kit";
-    public override string Description => "Party all night! - By Gindew v1.1";
+    public override string Description => "Party all night! - By Gindew";
     public override string IssueTrackerUrl => "https://x.com/RedGindew";
-    public override string TestedVersionRange => "[0.45.0]";
+    public override string TestedVersionRange => "[0.46.0]";
     private Dictionary<Point, TextureRegion> uiTextures;
+    private Dictionary<Point, TextureRegion> openShirt;
     public override TextureRegion Icon => this.uiTextures[new Point(0, 0)];
 
 
@@ -42,6 +43,7 @@ public class PartyNightKit : Mod
     {
         PartyNightKit.Logger = logger;
         texturePacker.Add(new UniformTextureAtlas(content.Load<Texture2D>("UITex"), 8, 8), r => this.uiTextures = r, 1, true);
+        texturePacker.Add(new UniformTextureAtlas(content.Load<Texture2D>("OpenShirt"), 4, 7), r => this.openShirt = r, 1, true, true);
     }
 
     public override void AddGameContent(GameImpl game, ModInfo info)
@@ -74,6 +76,24 @@ public class PartyNightKit : Mod
             Colors = new ColorSettings(ColorScheme.White, ColorScheme.Modern){Defaults = new int[] { 0, 11 }, PreviewName = "PartyNightKit.PartyWallBanner"},
             DefaultRotation = MLEM.Maths.Direction2.Right
         });
+        
+        FurnitureType.Register(new FurnitureType.TypeSettings("PartyNightKit.DiscoBall", new Point(1, 1), ObjectCategory.Lamp | ObjectCategory.SmallObject | ObjectCategory.NonColliding, 50, new ColorScheme[] { ColorScheme.White })
+        {
+            Icon = this.Icon,
+            Tab = (FurnitureTool.Tab.Lighting),
+            Colors = new ColorSettings(ColorScheme.White){ Defaults = new int[] { 0 } },
+            ElectricityRating = 1,
+            LightSettings = {
+                CreateLights = f => [
+                    new Light(f.Map, f.Position, f.Floor, Light.CircleTexture, new Vector2(6, 8), Color.GhostWhite) {
+                        VisualWorldOffset = new Vector2(0.5F)
+                    }
+                ]
+            }
+        });
+
+        Clothes.Register(new Clothes("PartyNightKit.OpenShirt", ClothesLayer.Shirt, this.openShirt, new Point(0, 0), 100, ClothesIntention.Everyday, StylePreference.Neutral, ColorScheme.WarmDarkMutedPastels) { Icon = this.Icon });
+
     }
 
     public override IEnumerable<string> GetCustomFurnitureTextures(ModInfo info)
@@ -82,5 +102,6 @@ public class PartyNightKit : Mod
         yield return "PartyRecordPlayer";
         yield return "PartyIcicleLight";
         yield return "PartyWallBanner";
+        yield return "DiscoBall";
     }
 }
